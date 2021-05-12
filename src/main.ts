8ed24165e9a8885393eaaee61c1ca272e1d7397b
AidenLx <aiden.lx@outlook.com>
2021-05-12 11:49:22 +0800

    refactor: add placeholder for opened MarkdownView refresh function

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { bridgeInfo } from "modules/renderChild";
 import {
   MarkdownPreviewRenderer,
   MarkdownView,
+  Notice,
   Plugin,
   TAbstractFile,
   TextFileView,
+  WorkspaceLeaf,
 } from "obsidian";
 import "./main.css";
 // import { BetterFnSettings, DEFAULT_SETTINGS, BetterFnSettingTab } from 'settings';
@@ -21,42 +23,56 @@ export default class BetterFn extends Plugin {
   onUnloadFileBak?: TextFileView["onUnloadFile"];
 
   /** Remove redundant element from fnInfo */
-  modifyOnUnloadFile = () => {
-    this.app.workspace.iterateAllLeaves((leaf) => {
-      if (
-        leaf.view instanceof MarkdownView &&
-        !(leaf.view.onUnloadFile as onUnloadFileModified).modified
-      ) {
-        const view = leaf.view;
-        const src = leaf.view.onUnloadFile;
-        if (!this.onUnloadFileBak) this.onUnloadFileBak = src;
-        view.onUnloadFile = (file) => {
-          // custom code here
-          const list = (
-            view.previewMode.containerEl.querySelector(
-              ".markdown-preview-section"
-            ) as BridgeEl
-          ).infoList;
-          if (list) list.length = 0;
+  modifyOnUnloadFile = (leaf: WorkspaceLeaf) => {
+    if (
+      leaf.view instanceof MarkdownView &&
+      !(leaf.view.onUnloadFile as onUnloadFileModified).modified
+    ) {
+      const view = leaf.view;
+      const src = leaf.view.onUnloadFile;
+      if (!this.onUnloadFileBak) this.onUnloadFileBak = src;
+      view.onUnloadFile = (file) => {
+        // custom code here
+        const list = (
+          view.previewMode.containerEl.querySelector(
+            ".markdown-preview-section"
+          ) as BridgeEl
+        ).infoList;
+        if (list) list.length = 0;
 
-          return src.bind(view)(file);
-        };
-        (view.onUnloadFile as onUnloadFileModified).modified = true;
-      }
-    });
+        return src.bind(view)(file);
+      };
+      (view.onUnloadFile as onUnloadFileModified).modified = true;
+    }
   };
 
-  revertOnUnloadFile = () => {
-    this.app.workspace.iterateAllLeaves((leaf) => {
-      if (leaf.view instanceof MarkdownView) {
-        if (
-          (leaf.view.onUnloadFile as onUnloadFileModified).modified &&
-          this.onUnloadFileBak
-        )
-          leaf.view.onUnloadFile = this.onUnloadFileBak.bind(leaf.view);
-      }
-    });
-  }
+  revertOnUnloadFile = (leaf: WorkspaceLeaf) => {
+    if (leaf.view instanceof MarkdownView) {
+      if (
+        (leaf.view.onUnloadFile as onUnloadFileModified).modified &&
+        this.onUnloadFileBak
+      )
+        leaf.view.onUnloadFile = this.onUnloadFileBak.bind(leaf.view);
+    }
+  };
+
+  /** refresh opened MarkdownView */
+  refresh = (leaf: WorkspaceLeaf) => {
+    // placeholder for now
+  };
+
+  /** get function to perform certain actions on all leaves */
+  doAllLeaves =
+    (...actions: ((leaf: WorkspaceLeaf) => void)[]) =>
+    () => {
+      this.app.workspace.iterateAllLeaves((leaf) => {
+        for (const action of actions) {
+          action(leaf);
+        }
+      });
+    };
+
+  layoutChangeCallback = this.doAllLeaves(this.modifyOnUnloadFile);
 
   async onload() {
     console.log("loading BetterFn");
@@ -64,8 +80,8 @@ export default class BetterFn extends Plugin {
     // await this.loadSettings();
 
     this.registerMarkdownPostProcessor(this.PopoverHandler);
-    this.modifyOnUnloadFile();
-    this.app.workspace.on("layout-change", this.modifyOnUnloadFile);
+    this.app.workspace.on("layout-change", this.layoutChangeCallback);
+    this.doAllLeaves(this.modifyOnUnloadFile, this.refresh)();
 
     // this.addSettingTab(new BetterFnSettingTab(this.app, this));
   }
@@ -74,8 +90,8 @@ export default class BetterFn extends Plugin {
     console.log("unloading BetterFn");
 
     MarkdownPreviewRenderer.unregisterPostProcessor(this.PopoverHandler);
-    this.app.workspace.off("layout-change", this.modifyOnUnloadFile);
-    this.revertOnUnloadFile();
+    this.app.workspace.off("layout-change", this.layoutChangeCallback);
+    this.doAllLeaves(this.revertOnUnloadFile, this.refresh)();
   }
 
   // async loadSettings() {
